13a7348b0797607c319a19c0d8bbc480e45c4655
Miguel P. Rodrigues <miguel.pinr@gmail.com>
2019-06-17 17:10:23 +0100

    Updates

---
+++ precache-manifest.ab6366a83c9037756630247855e1d155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182997d56a119d89051fd228c4691807",
+    "url": "/miguel/index.html"
+  },
+  {
+    "revision": "cf23b5e682fd23ac5fd8",
+    "url": "/miguel/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/miguel/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "cf23b5e682fd23ac5fd8",
+    "url": "/miguel/static/js/main.7db62adc.chunk.js"
+  },
+  {
+    "revision": "7ec0884a60a8a89918bb",
+    "url": "/miguel/static/js/runtime~main.2507b77f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miguel/static/media/logo.5d5d9eef.svg"
+  }
+]);
